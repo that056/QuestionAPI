b3be54b39b1a4bd44ded941650032a0a702265b4
Student <140450976+that056@users.noreply.github.com>
2024-10-12 16:41:39 +0200

    Update Questions.js

--- routes/Questions.js
+++ routes/Questions.js
@@ -1,42 +1,42 @@
-const express = require('express')
-const route = express.Router()
-const data = require('../info/data_Questions')
+const express = require('express');
+const route = express.Router();
+const data = require('../info/data_Questions');
+const { json } = require('body-parser');
 
-route.get('/',(req,res)=>{
-    let personal_Q =[]
-let skill_Q =[]
-let hobbie_q =[]
-let future_q =[]
-let user_questions=[]
-for (let i=0;i<data.length;i++){
-    if(data[i].category =='Future Goals'){
-        future_q.push(data[i])
-    }
-    else if(data[i].category =='Skills'){
-        skill_Q.push(data[i])
-    }
-    else if(data[i].category == 'Hobbies'){
-        hobbie_q.push(data[i])
-    }
-    else if(data[i].category == 'Personal'){
-        personal_Q.push(data[i])
-    }
-    
-}
-for(let i=0;i<2;i++){
-    const r = Math.round(Math.random() *4)
-    user_questions.push(hobbie_q[r])
-    const v = Math.round(Math.random() *4)
-    user_questions.push(skill_Q[v])
-    const x = Math.round(Math.random() *4)
-    user_questions.push(future_q[x])
-}
+route.get('/', (req, res) => {
+    let personal_Q = [];
+    let skill_Q = [];
+    let hobbie_q = [];
+    let future_q = [];
+    let user_questions = [];
 
-for(let i=0;i<2;i++){
-    const r = Math.round(Math.random() * personal_Q.length)
-    user_questions.push(personal_Q[r])
-}
-    res.json(user_questions)
-})
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].category == 'Future Goals') {
+            future_q.push(data[i]);
+        } else if (data[i].category == 'Skills') {
+            skill_Q.push(data[i]);
+        } else if (data[i].category == 'Hobbies') {
+            hobbie_q.push(data[i]);
+        } else if (data[i].category == 'Personal') {
+            personal_Q.push(data[i]);
+        }
+    }
 
-module.exports =route
+    for (let i = 0; i < 2; i++) {
+        const r = Math.round(Math.random() * 4);
+        user_questions.push(hobbie_q[r]);
+        const v = Math.round(Math.random() * 4);
+        user_questions.push(skill_Q[v]);
+        const x = Math.round(Math.random() * 4);
+        user_questions.push(future_q[x]);
+    }
+
+    for (let i = 0; i < 2; i++) {
+        const r = Math.round(Math.random() * personal_Q.length);
+        user_questions.push(personal_Q[r]);
+    }
+
+    res.json({ questions: user_questions });
+});
+
+module.exports = route;
