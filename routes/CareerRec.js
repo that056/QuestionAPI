04b03b89aa42212e2b141b062c9c5be3c3f3dcc2
Student <140450976+that056@users.noreply.github.com>
2024-10-13 20:48:00 +0200

    Update CareerRec.js

--- routes/CareerRec.js
+++ routes/CareerRec.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const route = express.Router()
-const careerRecommendations = require('../info/data_CareerRec')
+//const careerRecommendations = require('../info/data_CareerRec')
 
 
 
 route.post('/',(req,res)=>{
     console.log(req.body)
+    res.send('Got it')
    
 })
 
