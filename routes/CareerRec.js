1616a658465211ebbe2e465fed2d1fc86faee939
Student <140450976+that056@users.noreply.github.com>
2024-10-13 21:22:00 +0200

    Update CareerRec.js

--- routes/CareerRec.js
+++ routes/CareerRec.js
@@ -1,12 +1,31 @@
 const express = require('express')
 
 const route = express.Router()
-//const careerRecommendations = require('../info/data_CareerRec')
+const careerRecommendations = require('../info/data_CareerRec')
+let userSelections={}
 
+function RecommendCareer(userselection){
+    let recommendCareers= new Set()
+    for(const category in userSelections){
+       const selectedOption =  userSelections[category]
+       if(careerRecommendations[category][selectedOption]){
+        careerRecommendations[category][selectedOption].forEach(career =>{
+            recommendCareers.add(career)
+        })
+       }
 
+    }
+    return Array.from(recommendCareers)
+}
 
 route.post('/',(req,res)=>{
-    console.log(req.body)
+    const ar = req.body.response
+    for(const r in ar){
+       userSelections[ar[r].category] = ar[r].option
+    }
+    const rec_career = RecommendCareer(userSelections)
+    console.log(rec_career)
+
     res.send('Got it')
    
 })
