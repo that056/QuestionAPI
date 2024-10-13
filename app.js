4e19860da44083c91c192100f4a7f7e5dd7317be
Student <140450976+that056@users.noreply.github.com>
2024-10-13 17:35:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const { urlencoded } = require('body-parser')
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({extended:true}))
 
 
 app.use('/getquestions',require('./routes/Questions'))
+app.use('/submit',require('./routes/CareerRec'))
 
 app.listen(8007,()=>{
     console.log('server srunning')
