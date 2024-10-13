0931e38e92f828b596f8d69c31505f63efaf52b0
Student <140450976+that056@users.noreply.github.com>
2024-10-13 17:35:09 +0200

    Update CareerRec.js

--- routes/CareerRec.js
+++ routes/CareerRec.js
@@ -1,47 +1,13 @@
 const express = require('express')
+
 const route = express.Router()
-const careerMappings = require('../info/data_CareerRec')
-const userResponses = {
-    personal: [],
-    hobbies: [],
-    skills: [],
-    futureGoals: []
-};
+const careerRecommendations = require('../info/data_CareerRec')
 
 
-function recordResponse(category, questionIndex, selectedOption) {
-    userResponses[category][questionIndex] = selectedOption;
-}
 
-function recommendCareer() {
-    let recommendedCareers = [];
-
-    // Analyze personal, hobbies, skills, and future goals responses
-    Object.keys(userResponses).forEach(category => {
-        userResponses[category].forEach(response => {
-            const careers = careerMappings[response];
-            if (careers) {
-                recommendedCareers = recommendedCareers.concat(careers);
-            }
-        });
-    });
-
-    // Count the occurrences of each career
-    const careerCount = recommendedCareers.reduce((acc, career) => {
-        acc[career] = (acc[career] || 0) + 1;
-        return acc;
-    }, {});
-
-    // Sort careers by frequency
-    const sortedCareers = Object.entries(careerCount)
-        .sort((a, b) => b[1] - a[1])
-        .map(entry => entry[0]);
-
-    return sortedCareers;
-}
-
-route.get('/',(req,res)=>{
-    res.json(user_questions)
+route.post('/',(req,res)=>{
+    console.log(req.body)
+   
 })
 
 module.exports =route
