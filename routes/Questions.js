c8347c3cc17097f732563cdc7a47fbd0834e0ccf
Student <140450976+that056@users.noreply.github.com>
2024-10-14 18:07:19 +0200

    Update Questions.js

--- routes/Questions.js
+++ routes/Questions.js
@@ -23,15 +23,15 @@ route.get('/', (req, res) => {
     }
 
     for (let i = 0; i < 2; i++) {
-        const r = Math.round(Math.random() * 4);
+        const r = Math.round(Math.random() * hobbie_q.length);
         user_questions.push(hobbie_q[r]);
         const idx = hobbie_q.indexOf(hobbie_q[r])
         hobbie_q.splice(idx,1)
-        const v = Math.round(Math.random() * 4);
+        const v = Math.round(Math.random() * skill_Q.length);
         user_questions.push(skill_Q[v]);
         const idv = skill_Q.indexOf(skill_Q[v])
         skill_Q.splice(idv,1)
-        const x = Math.round(Math.random() * 4);
+        const x = Math.round(Math.random() * future_q.length);
         user_questions.push(future_q[x]);
         const ida = future_q.indexOf(future_q[x])
         future_q.splice(ida,1)
